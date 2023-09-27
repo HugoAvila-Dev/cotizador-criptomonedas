a118c3838684499fb96feedf363cff970dcbc1e1
Hugo <hhugo.avila97@gmail.com>
2023-09-27 10:02:42 -0400

    feat: ✨funcion para leer los select

--- js/app.js
+++ js/app.js
@@ -1,26 +1,38 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
 
 //Crear un Promise 
-const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
+const obtenerCriptomonedas = criptomonedas => new Promise(resolve => {
     resolve(criptomonedas);
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 });
 
 function consultarCriptomonedas() {
     const url = 'https://min-api-v2.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
 
     fetch(url)
-        .then( respuesta => respuesta.json() )
-        .then( resultado => obtenerCriptomonedas(resultado.Data))
-        .then( criptomonedas => selectCriptomonedas(criptomonedas))
+        .then(respuesta => respuesta.json())
+        .then(resultado => obtenerCriptomonedas(resultado.Data))
+        .then(criptomonedas => selectCriptomonedas(criptomonedas))
 
 }
 
-function selectCriptomonedas( criptomonedas ) {
-    criptomonedas.forEach ( cripto => {
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach(cripto => {
         const { FullName, Name } = cripto.CoinInfo;
         const option = document.createElement('option');
         option.value = Name;
@@ -28,4 +40,24 @@ function selectCriptomonedas( criptomonedas ) {
 
         criptomonedasSelect.appendChild(option);
     })
+}
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    //Validar
+    const { moneda, criptomonedas } = objBusqueda;
+
+    if(moneda === '' || criptomonedas === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+}
+
+function mostrarAlerta(mensaje) {
+    console.log(mensaje)
 }
